27650835cdfbe5cba46cb29e76ded077c8891e81
azeemuddinn <azeemuddin110@gmail.com>
2025-06-24 14:25:15 +0530

    update

--- core/story_generator.py
+++ core/story_generator.py
@@ -5,7 +5,7 @@ generator = pipeline('text-generation', model='gpt2')
 set_seed(42)
 
 def generate_story(description, mood=None):
-    # return f"A story about: {description}. Mood: {mood or 'default'}."
+    return f"A story about: {description}. Mood: {mood or 'default'}."
     prompt = f"Here is  a short 100-word {mood + ' ' if mood else ''}story about: {description}"
     output = generator(prompt, max_length=120, num_return_sequences=1)[0]['generated_text']
     
